cf2a982b9725ed7f91c7c6c278478b9378e74ab4
Aarti Chella <aartichella@gmail.com>
2019-03-27 10:36:37 -0400

    Updated 27/03

---
+++ SplitViewExample/Data.swift
@@ -0,0 +1,22 @@
+//
+//  Data.swift
+//  SplitViewExample
+//
+//  Created by Aarti Chella on 3/26/19.
+//  Copyright © 2019 aarti. All rights reserved.
+//
+
+import UIKit
+
+let camera = UIImage(named: "1")
+let city = UIImage(named: "2")
+let animals = UIImage(named: "3")
+let flowers = UIImage(named: "4")
+let stand = UIImage(named: "5")
+let urban = UIImage(named: "6")
+
+let imagesArray = [camera, city, animals, flowers, stand, urban]
+
+let namesArray = ["CAMERA", "CITY", "ANIMALS", "FLOWERS", "STAND", "URBAN"]
+
+let colorsArray = [#colorLiteral(red: 1, green: 0.1918507635, blue: 0.1281559252, alpha: 1), #colorLiteral(red: 0, green: 0.5898008943, blue: 1, alpha: 1), #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1), #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1), #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1), #colorLiteral(red: 0.5787474513, green: 0.3215198815, blue: 0, alpha: 1)]
